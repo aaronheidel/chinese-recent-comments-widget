678639546a5f39daf5e5147aa9af2b4748f3e0a2
aaronheidel <aaronheidel@gmail.com>
2016-11-20 22:10:48 +0800

    Update recent-comments-widget.js

--- recent-comments-widget.js
+++ recent-comments-widget.js
@@ -52,66 +52,60 @@ function showrecentcomments(json) {
 	//	document.write('rc_comment_italics=' + rc_comment_italics + '</br>');
 	//	document.write('json.feed.entry.length=' + json.feed.entry.length + '</br>');
 
-	if (1) {
-		document.write('<div class="rcw-comments">');
-		document.write('Blah!</p>');
-		document.write('</div>');
-	} else {
-		for (var i = 0; i < rc_comment_count; i++) {
-		//	document.write('i=' + i + '</br>');
-			var rc_entry = json.feed.entry[i];   // rc_entry is current entry
-			if (i == json.feed.entry.length)
-				break;
-			var post_url = get_comment_link(rc_entry);
+	for (var i = 0; i < rc_comment_count; i++) {
+	//	document.write('i=' + i + '</br>');
+		var rc_entry = json.feed.entry[i];   // rc_entry is current entry
+		if (i == json.feed.entry.length)
+			break;
+		var post_url = get_comment_link(rc_entry);
 
-			// if post_url is empty, skip this comment
-			if (post_url == '') {
-				rc_comment_count++;
-				continue;
-			}
-
-			var comment_url = post_url.split("#");
-			   // yields comment_url[0] = "http://myblog.blogspot.com/2013/01/blog-post_20.html?showComment=1395647023572"
-			   //        comment_url[1] = "c646752016000786413"
-			comment_url = comment_url[0];                   // yields top of entry page containing comment
-			var post_link = extract_post_title_from_url(comment_url).link(comment_url);
-					// <a href="http://myblog.blogspot.com/2013/01/blog-post_20.html?showComment=1395647023572">blog post_20</a>
-			if ("content" in rc_entry) {
-				var contents = rc_entry.content.$t;   // contents of comment
-			} else if ("summary" in rc_entry) {
-				var contents = rc_entry.summary.$t;
-			} else 
-				var contents = "";
-			var regex = /<\S[^>]*>/g;               // catches any HTML tag
-			contents = contents.replace(regex,"");          // erase HTML tags
-
-			// replace author name if necessary with 版主 or 匿名
-			var comment_author = rc_entry.author[0].name.$t;
-			if (comment_author == json.feed.author[0].name.$t) comment_author = '版主';
-			if (comment_author == json.feed.title.$t) comment_author = '版主';
-			if (comment_author == 'Anonymous') comment_author = '匿名';
-
-			document.write('<div class="rcw-comments">');
-			// write comment
-			if(rc_comment_chars > 0) {
-				document.write('<a href="' + post_url + '">');
-				if (rc_comment_italics) document.write('<i>');
-				document.write( summarize_comment(contents, rc_comment_chars) );
-				if (rc_comment_italics) document.write('</i>');
-				document.write('</a>');
-			}
-
-			// write author (if no comment is shown, make the author's name a link)
-			document.write(' <span class="author-rc">');
-			if (rc_comment_chars == 0) document.write('<a href="'+post_url+'">');
-			document.write(comment_author);
-			if (rc_comment_chars == 0) document.write('</a>');
-			document.write('</span>');
-
-			// write date/time
-			if (rc_show_datetime == true)  // display comment date?
-				document.write(' ' + humanize_datetime(rc_entry.published.$t));
-			document.write('</div>');
+		// if post_url is empty, skip this comment
+		if (post_url == '') {
+			rc_comment_count++;
+			continue;
 		}
+
+		var comment_url = post_url.split("#");
+		   // yields comment_url[0] = "http://myblog.blogspot.com/2013/01/blog-post_20.html?showComment=1395647023572"
+		   //        comment_url[1] = "c646752016000786413"
+		comment_url = comment_url[0];                   // yields top of entry page containing comment
+		var post_link = extract_post_title_from_url(comment_url).link(comment_url);
+				// <a href="http://myblog.blogspot.com/2013/01/blog-post_20.html?showComment=1395647023572">blog post_20</a>
+		if ("content" in rc_entry) {
+			var contents = rc_entry.content.$t;   // contents of comment
+		} else if ("summary" in rc_entry) {
+			var contents = rc_entry.summary.$t;
+		} else 
+			var contents = "";
+		var regex = /<\S[^>]*>/g;               // catches any HTML tag
+		contents = contents.replace(regex,"");          // erase HTML tags
+
+		// replace author name if necessary with 版主 or 匿名
+		var comment_author = rc_entry.author[0].name.$t;
+		if (comment_author == json.feed.author[0].name.$t) comment_author = '版主';
+		if (comment_author == json.feed.title.$t) comment_author = '版主';
+		if (comment_author == 'Anonymous') comment_author = '匿名';
+
+		document.write('<div class="rcw-comments">');
+		// write comment
+		if(rc_comment_chars > 0) {
+			document.write('<a href="' + post_url + '">');
+			if (rc_comment_italics) document.write('<i>');
+			document.write( summarize_comment(contents, rc_comment_chars) );
+			if (rc_comment_italics) document.write('</i>');
+			document.write('</a>');
+		}
+
+		// write author (if no comment is shown, make the author's name a link)
+		document.write(' <span class="author-rc">');
+		if (rc_comment_chars == 0) document.write('<a href="'+post_url+'">');
+		document.write(comment_author);
+		if (rc_comment_chars == 0) document.write('</a>');
+		document.write('</span>');
+
+		// write date/time
+		if (rc_show_datetime == true)  // display comment date?
+			document.write(' ' + humanize_datetime(rc_entry.published.$t));
+		document.write('</div>');
 	}
 }
